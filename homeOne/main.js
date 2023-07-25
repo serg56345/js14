e1003b4e16c8de8d036abd27b2d481a115066230
serg56345 <136841711+serg56345@users.noreply.github.com>
2023-07-25 22:13:46 +0300

    Add files via upload

---
+++ homeOne/main.js
@@ -0,0 +1,48 @@
+  /* //Number:age//
+     let date =  prompt ("Скільки тобі років?");
+      alert(`Ти народився(-лася) у ${2023-date} році:) `)*/
+
+
+     /*  //Number:temperature// 
+      let temperature = prompt( "Яка температура в Цельсіях?");
+       let a=(temperature * 2)+30;
+       a=Math.floor((temperature * 2)+30);
+       alert (a);*/
+      
+     
+
+      
+
+         /*//Number: currency//
+         let a =prompt("Долар");
+         const rate = 35.5;
+         alert( rate * a);
+         let b =prompt("Євро");
+         const rateOne = 40;
+         alert( rateOne * b);
+         let c =prompt("Марки");
+         const rateTwo = 3;
+         alert( rateTwo* c);*/
+         
+         
+             /*b =prompt("Євро");
+             c = prompt("Mark")
+        const rate= 35.5;
+         alert( rate * a);*/
+
+
+          /*//Number:flats//
+         let a = prompt ("Введіть кількість поверхів");
+             b =  prompt("Введіть кількість квартир на поверсі");
+             c =  prompt("Введіть номер квартири");
+            let d =c/(a*b);
+            d=Math.ceil(c/(a*b))
+             alert(`У вас ${d}  під'їзд ` );
+             let m= (c-((d-1)*a*b))/b;
+             m=Math.ceil((c-((d-1)*a*b))/b);
+             alert( `У вас ${m}  поверх `);*/
+
+
+     
+     
+    
