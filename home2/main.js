47365a8cefcd823ccc80338dd228902dd9c267bd
serg56345 <136841711+serg56345@users.noreply.github.com>
2023-07-25 22:16:59 +0300

    Add files via upload

---
+++ home2/main.js
@@ -0,0 +1,50 @@
+/*//String: greeting//
+let str = prompt("Як вас звати?")
+alert(`Вітаю ${str}`)*/
+
+/*//String: gopni4ek
+let str =  prompt("напишіть про себе");
+a = str.split("").join("блін");
+console.log(a)*/
+
+/* //String: capitalize
+let str = "cANBerRa";
+let result =  str.slice(0,1).toUpperCase()+str.slice(1,7).toLowerCase();
+console.log(result)*/
+
+/*//String: word count
+let str = "hello world!!!";
+a = str.replace(/ /g,('')).length;
+console.log(a)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
