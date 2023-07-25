1cd13c2bd892d1d8c0712fa90a37cbc59f1fb7d3
serg56345 <136841711+serg56345@users.noreply.github.com>
2023-07-25 22:21:07 +0300

    Add files via upload

---
+++ home3/main.js
@@ -0,0 +1,86 @@
+/*//Number: odd
+let  digits = Number (prompt(" введіть число"));
+    if (digits % 2 == 0){
+        alert("Вітаю, ви ввели парне число число")
+    } else alert("Вам неоюхідно ввести  парне число")*/
+
+
+   /* //Boolean
+    let a = confirm("Готовий відповідати на запитання");
+    console.log(a)*/
+
+
+   /* //Boolean: if
+    let a = confirm("Готовий відповідати на запитання");
+    if ( a){
+        alert("ok");
+    } else if (confirm("Отже, ви не готові відповідати на запитання?")){
+        alert("ok");
+    }else  (alert("я вас розумію"))*/
+
+    /*//Prompt: or
+    let age = Number(prompt("Введіть свій вік"))
+    if(age){
+        alert(`Вам ${age} років`)
+    }
+    else if (""||prompt!=Number){
+        alert("Ви допустили помилку")
+    }*/
+
+    /* //Prompt: or
+    let age = Number(prompt("Введіть свій вік"));
+    if ( age ){alert (`Вам ${age} років`)
+}else if(age !=Number){
+    alert(`Ви ввели некоректно число`)
+}*/
+
+ /*//Confirm: or this days
+  let ask =prompt("Шопінг?")
+  if(ask){
+    alert("Го шопити")
+ } else (alert("Бяка"))*/
+
+  
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+    
+    
+    
+
+    
+    
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
