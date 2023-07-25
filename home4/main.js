692436b4d616bf42993cbe26a0e8841e9a45f3c0
serg56345 <136841711+serg56345@users.noreply.github.com>
2023-07-25 22:22:26 +0300

    Add files via upload

---
+++ home4/main.js
@@ -0,0 +1,128 @@
+/*//Confirms
+let answerConfirm =new Array(["так", "ні", "шопінг", "купити","продати"]);*/
+
+/*//Prompts
+let answerPrompt = ["число", "null", "usd", "eur", "Іванов", "Іван","Іванович","Вік", "температура", "red","Gren","Blue"];
+console.log(answerPrompt[5])*/
+
+/*//Item access
+let answerPrompt = ["число", "null", "usd", "eur", "Іванов", "Іван","Іванович","Вік", "температура", "red","Gren","Blue"];
+let index = Number(prompt("Введіть індекс у масиві<=11"));
+if(index<=11){
+    console.log(answerPrompt[index])
+}else (alert("Вам необхідно ввести число не більше 12"));*/
+/*//Multiply table
+const arr = [
+    [1,2,3,4,5], 
+    [2,4,6,8,10], 
+    [3,6,9,12,15], 
+    [4,8,12,16,20],
+    [5,10,15,20,25]
+]
+console.log(arr)*/
+
+/*//Item change
+let index = Number(prompt("Введіть індекс у масиві"));
+let indexVelue = prompt("Введіть значення для цього індексу");
+ let arr = [indexVelue]
+ console.log(arr)*/
+
+ 
+/*//Reverse
+     let arr =[]
+   arr.push(prompt(" Прізвище  "));
+    arr.push(prompt(" Ім'я "));
+    arr.push(prompt("По батькові"));
+    let   a   = arr.pop()
+    let   b   = arr.pop()
+    let   c   = arr.pop()
+     console.log(arr, a, b, c)*/
+
+    /* //Reverse 2
+    let arr =[]
+    arr.push(prompt(" Прізвище  "));
+    arr.push(prompt(" Ім'я "));
+    arr.push(prompt("По батькові"));
+    let   a   = arr.pop();
+    let   b   = arr.pop();
+    let   c   = arr.pop();
+    let arrOne = [a,b,c,];
+        arrOne.shift()
+    let arrTwo = arrOne
+        arrTwo.shift()
+    let arrTree =arrTwo
+    arrTree.shift()
+    let arrFor = arrTree;
+    arrFor.unshift(c,b,a)
+    console.log(arrFor)*/
+
+   /*//Copy
+    const arr = [
+        [1,2,3,4,5], 
+        [2,4,6,8,10], 
+        [3,6,9,12,15], 
+        [4,8,12,16,20],
+        [5,10,15,20,25]
+    ]
+    let arrCopy =arr;
+    console.log(...arrCopy)*/
+
+    /*//Deep Copy
+    const arr = [
+        [1,2,3,4,5], 
+        [2,4,6,8,10], 
+        [3,6,9,12,15], 
+        [4,8,12,16,20],
+        [5,10,15,20,25]
+    ]
+    arrClone = structuredClone(arr)
+    console.log(arrClone)*/
+
+
+   /* //Flat
+    const arr = [
+        [1,2,3,4,5], 
+        [2,4,6,8,10], 
+        [3,6,9,12,15], 
+        [4,8,12,16,20],
+        [5,10,15,20,25]
+    ]
+     console.log(arr.flat().length)*/
+
+
+    /* //Destruct
+     let string = "двадцятий"
+     console.log(string[0])
+     console.log(string[4])
+     console.log(string[8])*/
+
+     /*//For Alert
+     let arr = ["John", "Paul", "George", "Ringo"]
+     for(let i = 0; i<arr.length; i++){
+        alert(arr[i])
+     }*/
+
+     /*const currencies = ["USD", "EUR", "GBP", "UAH"]
+     let   str = "<select>"
+     for (const currency of currencies){
+    
+    }
+     str+= "</select>"
+     document.write(str) //document.write відобразить ваш HTML на сторінці*/
+     
+     
+
+    
+  
+
+    
+     
+    
+    
+    
+    
+    
+    
+
+  
+
