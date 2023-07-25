56ca018ebe9929d95342446fd42cceceea766bc1
serg56345 <136841711+serg56345@users.noreply.github.com>
2023-07-25 22:23:39 +0300

    Add files via upload

---
+++ home5/main.js
@@ -0,0 +1,52 @@
+/*//Literals
+const car = {
+    year : "2002",
+    color : "red",
+    car_brend : "BMW", 
+}*/
+
+
+/*//Literals expand
+const car = {
+    year : "2002",
+    color : "red",
+    car_brend : "BMW",
+    body_type : prompt("Введіть тип кузова" ) 
+}
+console.log(car)*/
+/*//Destruct array
+let arr = [1,2,3,4,5, "a", "b", "c"]
+let [odd1, even1, odd2, even2, odd3, ...rest] = arr;
+console.log(even2)*/
+
+/*//Destruct string
+let arr = [1, "abc"]
+let [number, s] = arr
+let[s1,s2,s3,] = s.slice(',')
+console.log(s3)*/
+
+/*//Destruct 2
+let obj = {name: 'Ivan',
+           surname: 'Petrov',
+           children: [{name: 'Maria'}, {name: 'Nikolay'}]}
+
+let [{name: name1}, {name:name2}] = obj.children;
+console.log(name2)*/
+/*//Destruct 3
+let arr = [1,2,3,4, 5,6,7,10]
+let {0:a, 1:b, length} = arr
+console.log(length)*/
+
+/*//Form
+const user = {
+    "Name":"Ваше ім'я",
+    "Surname":"Ваше прізвище",
+    "Age":"Скільки вам років?",
+    "Country":"Країна в якій проживаєш",
+    "Nationality":"Національність",
+    "Profession":"Ваша професія",
+    "Adress": "Домашня адреса"
+}*/
+
+
+
